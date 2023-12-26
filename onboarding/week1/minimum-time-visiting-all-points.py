0f58beb1c8e6cda6d7b684eb0eba5ed38a26ae75
Kalki767 <137619853+Kalki767@users.noreply.github.com>
2023-12-26 13:29:23 +0300

    Added solution for Minimum Time Visiting All Points

--- onboarding/week1/minimum-time-visiting-all-points.py
+++ onboarding/week1/minimum-time-visiting-all-points.py
@@ -4,7 +4,5 @@ class Solution:
         for i in range(len(points)-1):
             x_inc = abs(points[i][0]-points[i+1][0])
             y_inc = abs(points[i][1]-points[i+1][1])
-            diagonal = min(x_inc,y_inc)
-            total+=diagonal
-            total = total+ max(x_inc,y_inc)-diagonal
+            total+= max(x_inc,y_inc) 
         return total
