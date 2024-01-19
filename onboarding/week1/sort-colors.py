cc0fdcced77bb8f9b24c4ebbe6cdcd981835af60
Kalki767 <137619853+Kalki767@users.noreply.github.com>
2024-01-19 10:52:49 +0300

    Added solution for Sort Colors

--- onboarding/week1/sort-colors.py
+++ onboarding/week1/sort-colors.py
@@ -3,12 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #using selection sort to sort the array
-        n = len(nums)
-        for i in range(n):
-            min_indx = i
-            for j in range(i+1,n):
-                if nums[j]<nums[min_indx]:
-                    min_indx = j
-            nums[min_indx],nums[i] = nums[i],nums[min_indx]
-        
+        #Step1: assign one pointer to the beginning of the array
+        left=0
+
+        #Step2: iterate through the array until the left pointer reaches the second last element
+        while left<len(nums)-1:
+
+            #Step3: assign another pointer right which is the left pointer incremented by one and check if the element at that pointer is bigger or not
+            right = left+1
+            while right<len(nums):
+                if nums[left]>nums[right]:
+                    nums[left],nums[right]=nums[right],nums[left]
+                if nums[left]==0:
+                    break
+                right+=1
+            left+=1
+    
