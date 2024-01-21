3369019557065fd80ccc7408d8422c02d3fa325a
Kalki767 <137619853+Kalki767@users.noreply.github.com>
2024-01-21 19:57:10 +0300

    Added solution for Longest Subarray of 1's After Deleting One Element

--- onboarding/week1/longest-subarray-of-1s-after-deleting-one-element.py
+++ onboarding/week1/longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,11 +1,15 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
+        #Approach: Sliding window
         n = len(nums)
+        #Step1: check for edge cases if all elements are 1 return n-1
         if sum(nums)==n:
             return n-1
+        #Step2: assign two pointers to the start of the array and set max_length to zero
         left=right=0
         max_length=0
         count_of_zero =0
+        #Step3: iterate through the array while checking if the count_of_zero is either less than one or equal to one if it's then calculate max_length by right-left-1 if not just increment the right pointer
         while right<n:
             if count_of_zero<1:
                 if nums[right]==0:
@@ -20,5 +24,6 @@ class Solution:
                         left+=1
                 else:
                     right+=1
+        #Step4: last check if the array goes out of bound before calculating the max_length
         max_length = max(max_length,right-left-1)
         return max_length
